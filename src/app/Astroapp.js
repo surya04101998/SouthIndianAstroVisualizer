84140f838f2aae4dff4378526e34ed26fe2285a7
surya04101998 <40798305+surya04101998@users.noreply.github.com>
2023-07-31 17:24:29 +0000

    starter

---
+++ src/app/Astroapp.js
@@ -0,0 +1,10 @@
+import Grid from '../common/Grid'
+const arrOfObj = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+function AstroApp(props){
+    var ans=do_smth()
+return <a><Grid arrOfObj={arrOfObj}/></a>
+}
+function do_smth(){
+    return "wow"
+}
+export default AstroApp;
